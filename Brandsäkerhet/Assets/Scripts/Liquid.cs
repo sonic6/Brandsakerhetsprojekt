4c8d87bf41a525b087ad6cb5b01a2dd51d40a380
sonic6 <alshihabi.ahmad@gmail.com>
2019-05-14 21:52:53 +0200

    Added liquid script

---
+++ Brandsäkerhet/Assets/Scripts/Liquid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liquid : MonoBehaviour
+{
+    [SerializeField] GameObject LiquidUnit;
+    [SerializeField] Transform bottleCap;
+    [SerializeField] float time;
+    [SerializeField] float timeMax;
+
+    void Update()
+    {
+        time = time + 1 * Time.deltaTime;
+
+        if ((bottleCap.eulerAngles.x >= 90 && bottleCap.eulerAngles.x <= 270) || (bottleCap.eulerAngles.z >= 90 && bottleCap.eulerAngles.z <= 270))
+        {
+            if (time >= timeMax)
+            {
+                Instantiate(LiquidUnit, transform.position, transform.rotation);
+                time = 0;
+            }
+        }
+    }
+}
