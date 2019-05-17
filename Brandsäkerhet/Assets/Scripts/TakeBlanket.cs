ede579979e87c3a30f082525c351508061da999d
sonic6 <alshihabi.ahmad@gmail.com>
2019-05-17 15:55:46 +0200

    Added new scripts
    one script will make an object start burning after a given amount of seconds
    
    the other script will make the fire blanket get drawn out of the fire blanket holder using a VR controler

---
+++ Brandsäkerhet/Assets/Scripts/TakeBlanket.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class TakeBlanket : MonoBehaviour
+{
+    [SerializeField] GameObject fireBlanket;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.parent.transform.parent.GetComponent<VRTK_ControllerEvents>().triggerClicked == true) //This will check if the trigger button of the VR controller that's colliding is all the way down
+        {
+            fireBlanket.SetActive(true);
+        }
+    }
+}
