ccfaab8591f0c9cc974bec52a12eea96f874a692
AustrianBasementDaddy <wardenoath@hotmail.com>
2019-05-21 15:36:44 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/sonic6/Brandsakerhetsprojekt""
    This reverts commit 4f924eed7dfb515360cea0c3622135145156668c.

---
+++ Brandsäkerhet/Assets/Scripts/BurnAfterTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BurnAfterTime : MonoBehaviour
+{
+    public bool activateScenario;
+    [SerializeField] float startBurningAfterSeconds;
+    private GameObject myFire;
+
+    private void Awake()
+    {
+        myFire = transform.GetComponentInChildren<ParticleSystem>().gameObject;
+        myFire.SetActive(false);
+    }
+
+    void Start()
+    {
+        if (activateScenario == true)
+        {
+            StartCoroutine(BurnNow());
+        }
+    }
+
+    IEnumerator BurnNow()
+    {
+        yield return new WaitForSeconds(startBurningAfterSeconds);
+        myFire.SetActive(true);
+    }
+    
+}
---
+++ Brandsäkerhet/Assets/Scripts/TakeBlanket.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class TakeBlanket : MonoBehaviour
+{
+    [SerializeField] GameObject fireBlanket;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponentInParent<VRTK_ControllerEvents>().triggerPressed == true)
+        { 
+            fireBlanket.SetActive(true);
+            other.GetComponentInParent<VRTK_InteractGrab>().AttemptGrab();
+        }
+    }
+}
