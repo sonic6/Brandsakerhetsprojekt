e3fa2c33c9915ba6a6abd25b2b2b407c3a43999e
sonic6 <alshihabi.ahmad@gmail.com>
2019-06-03 03:43:52 +0200

    boyaaa, jag kan sova nu

--- Brandsäkerhet/Assets/Scripts/BurnAfterTime.cs
+++ Brandsäkerhet/Assets/Scripts/BurnAfterTime.cs
@@ -6,7 +6,9 @@ public class BurnAfterTime : MonoBehaviour
     public bool activateScenario;
     [SerializeField] float startBurningAfterSeconds;
     private GameObject myFire;
+    private GameObject mySmoke;
     SmokeBuildUp mySmokeBuilder;
+    [SerializeField] SoundHandler alarmSound;
 
     private void Awake()
     {
@@ -14,6 +16,9 @@ public class BurnAfterTime : MonoBehaviour
         mySmokeBuilder.enabled = false;
         myFire = transform.GetComponentInChildren<ParticleSystem>().gameObject;
         myFire.SetActive(false);
+        mySmoke = transform.GetComponentInChildren<ParticleSystem>().gameObject;
+        mySmoke.SetActive(false);
+        alarmSound.enabled = false;
     }
 
     void Start()
@@ -28,7 +33,9 @@ public class BurnAfterTime : MonoBehaviour
     {
         yield return new WaitForSeconds(startBurningAfterSeconds);
         myFire.SetActive(true);
+        mySmoke.SetActive(true);
         mySmokeBuilder.enabled = true;
+        alarmSound.enabled = true;
     }
     
 }
